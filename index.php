c5ba40f4629552df964a73c91b1ecc9340c5e7bc
VENTURA\Atila <atilagermano@hotmail.com>
2024-08-01 23:11:13 -0300

    imagens logo

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>ClubCar</title>
+    <link rel="shortcut icon" type="image/x-icon" href="img/logtitle.png">
 </head>
 <body>
     <div class="login">
+        
         <div class="form">
             <form>
+            <img src="img/logo.png" class="imagem">
                 <input type="email" name="usuario" placeholder="Email">
                 <input type="password" name="senha" placeholder="Senha">
                 <button>Login</button>
